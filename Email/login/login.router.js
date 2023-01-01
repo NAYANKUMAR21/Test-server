dfca0a6f3c80f1ee84737b25371ecfa082c77809
Nayan Kumar <naayaankumar@gmail.com>
2023-01-01 20:47:07 +0530

    updated argon changes

--- Email/login/login.router.js
+++ Email/login/login.router.js
@@ -2,15 +2,16 @@ const express = require("express");
 const app = express.Router();
 const argon2 = require("argon2");
 const loginModel = require("./login.model");
-const { find } = require("../Emial.model");
 app.post("/login", async (req, res) => {
+  console.log(req.body);
   const { email, password } = req.body;
   if (!email || !password) {
     return res.status(404).send("Login Falied");
   }
   try {
     const user = await loginModel.findOne({ email: email });
-    if (user && (await argon2.verify(user.password, "password"))) {
+    let x = await argon2.verify(user.password, password);
+    if (user && x) {
       return res.status(201).send(true);
     } else {
       return res.status(403).send("Un-Authorized");
@@ -26,8 +27,8 @@ app.post("/signup", async (req, res) => {
     return res.status(404).send("Login Falied");
   }
   try {
-    const hash = await argon2.hash("password");
-    console.log(hash);
+    const hash = await argon2.hash(password);
+    // console.log(hash);
     const newUSer = await loginModel({ email, password: hash });
     await newUSer.save();
     return res.status(201).send("User Sucessfully created");
